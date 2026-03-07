ffec34a5aaaa88645af2ff0989210a05062383f7
Devanshu Kumar <thepixelpoet1@gmail.com>
2026-03-07 18:50:40 +0530

    privacy and terms

--- backend/index.js
+++ backend/index.js
@@ -8,6 +8,7 @@ const app = express();
 // Middleware
 app.use(cors());
 app.use(express.json());
+app.use(express.static('public')); // Serve static files from 'public' folder
 
 // Request logger
 app.use((req, res, next) => {
